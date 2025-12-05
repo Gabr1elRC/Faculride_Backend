ed8315932575567f76094bb07e09c0d4ca2f1c9b
Gabr1elRC <gabriel.correa25@fatec.sp.gov.br>
2025-12-05 10:47:49 -0300

    test sonar

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,18 +4,41 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  dialect: "postgres",
-  dialectModule: pg,
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  logging: false
+console.log("🔍 DB_ENV:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  db: process.env.DB_DATABASE,
 });
+
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_DATABASE
+) {
+  throw new Error("❌ Variáveis de ambiente do banco não estão definidas corretamente.");
+}
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+    dialectModule: pg,
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  }
+);
 
 export default sequelize;
 
